f7800ae2c88f82beeffda5628a9f9ebc9c2e87cc
vic <finalsteve@hotmail.com>
2015-03-01 19:11:45 +0800

    update

--- 20140225_2/src/page/tech/TechUltra.hx
+++ 20140225_2/src/page/tech/TechUltra.hx
@@ -1,4 +1,8 @@
 package page.tech;
+import flash.display.DisplayObject;
+import flash.display.DisplayObjectContainer;
+import flash.display.MovieClip;
+import org.vic.utils.BasicUtils;
 
 /**
  * ...
@@ -6,11 +10,33 @@ package page.tech;
  */
 class TechUltra extends DefaultTechPage
 {
+	private var _mc_controller:MovieClip;
+	private var _mc_slider:DisplayObject;
+	private var _mc_mask:DisplayObject;
 
 	public function new() 
 	{
 		super();
 		this.createDebugRoot("ultra");
+	}
+	
+	override function onOpenEvent(param:Dynamic, cb:Void->Void):Void 
+	{
+		super.onOpenEvent(param, cb);
+		
+		BasicUtils.revealObj( getRoot(), function( obj:DisplayObject ) {
+			switch( obj.name ) {
+				case 'mc_controller':
+					_mc_controller = cast( obj, MovieClip );
+			}
+		});
+		
+		//x 539~889
+		_mc_controller.gotoAndStop( 50 );
+		//_mc_slider = _mc_controller.getChildByName( 'mc_slider' );
+		//_mc_mask = _mc_controller.getChildByName( 'mc_mask' );
+		//_mc_slider.x = _mc_mask.x = 100;
+		//trace( _mc_slider.x );
 	}
 	
 	override function getSwfInfo():Dynamic 
