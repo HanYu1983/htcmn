d9fbc4baa378ed5de301cc5bd30b882505470025
vic <finalsteve@hotmail.com>
2015-03-03 01:05:56 +0800

    update

--- 20140225_2/src/page/tech/DefaultTechPage.hx
+++ 20140225_2/src/page/tech/DefaultTechPage.hx
@@ -1,8 +1,12 @@
 package page.tech;
+import flash.display.DisplayObject;
+import flash.display.DisplayObjectContainer;
+import flash.display.MovieClip;
 import haxe.remoting.FlashJsConnection;
 import helper.IResize;
 import helper.TechFramePage;
 import helper.Tool;
+import org.vic.utils.BasicUtils;
 
 /**
  * ...
@@ -10,6 +14,9 @@ import helper.Tool;
  */
 class DefaultTechPage extends DefaultPage
 {
+	var _mc_back:DisplayObject;
+	var _mc_item:MovieClip;
+	var _mc_person:DisplayObject;
 
 	public function new() 
 	{
@@ -18,7 +25,15 @@ class DefaultTechPage extends DefaultPage
 	}
 	
 	override public function onResize(x:Int, y: Int, w:Int, h:Int) {
-		Tool.center(this, x, y, w, h);
+		//Tool.center(this, x, y, w, h);
+		
+		if( _mc_item != null ){
+			Tool.center( _mc_item, x, y, w, h );
+		}
+		
+		if ( _mc_person != null ) {
+			Tool.centerY( _mc_person, y, h );
+		}
 	}
 	
 	override function onOpenEvent(param:Dynamic, cb:Void->Void):Void 
@@ -27,5 +42,16 @@ class DefaultTechPage extends DefaultPage
 		var frame = cast(getWebManager().getPage(TechFrame), TechFrame);
 		var clz = Type.getClass(this);
 		frame.animateButtonByTechPage(clz);
+		
+		BasicUtils.revealObj( getRoot(), function( obj:DisplayObject ) {
+			switch( obj.name ) {
+				case 'mc_back':
+					_mc_back = obj;
+				case 'mc_item':
+					_mc_item = cast( obj, MovieClip );
+				case 'mc_person':
+					_mc_person = obj;
+			}
+		});
 	}
 }
