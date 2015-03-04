3441556d02ca5041422fa778d3ea8d64ccb8fb8d
vic <finalsteve@hotmail.com>
2015-03-05 00:47:40 +0800

    update

--- 20140225_2/src/page/tech/DefaultTechPage.hx
+++ 20140225_2/src/page/tech/DefaultTechPage.hx
@@ -14,8 +14,6 @@ import org.vic.utils.BasicUtils;
  */
 class DefaultTechPage extends DefaultPage
 {
-	var _mc_back:DisplayObject;
-	var _mc_item:DisplayObject;
 	var _mc_person:DisplayObject;
 
 	public function new() 
@@ -25,18 +23,9 @@ class DefaultTechPage extends DefaultPage
 	}
 	
 	override public function onResize(x:Int, y: Int, w:Int, h:Int) {
-
-		if( _mc_item != null ){
-			Tool.centerForce( _mc_item, 1366, 768, x, y, w, h );
-		}
-		
+		super.onResize(x, y, w, h );
 		if ( _mc_person != null ) {
 			Tool.centerForceY( _mc_person, 768, y, h );
-		}
-		
-		if ( _mc_back != null ) {
-			_mc_back.width = w;
-			_mc_back.height = h;
 		}
 	}
 	
@@ -48,10 +37,6 @@ class DefaultTechPage extends DefaultPage
 		
 		BasicUtils.revealObj( getRoot(), function( obj:DisplayObject ) {
 			switch( obj.name ) {
-				case 'mc_back':
-					_mc_back = obj;
-				case 'mc_item':
-					_mc_item = obj;
 				case 'mc_person':
 					_mc_person = obj;
 			}
