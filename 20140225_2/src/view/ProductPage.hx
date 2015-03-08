49a7845133c0003d2a9adc8593ea782cf4a987c7
HanYu1983 <hanyushome@gmail.com>
2015-03-09 02:28:43 +0800

    update

--- 20140225_2/src/view/ProductPage.hx
+++ 20140225_2/src/view/ProductPage.hx
@@ -1,4 +1,5 @@
 package view;
+import helper.Tool;
 import org.vic.web.WebView;
 
 /**
@@ -26,5 +27,19 @@ class ProductPage extends DefaultPage
 	override function suggestionEnableAutoBarWhenOpen():Null<Bool> 
 	{
 		return false;
+	}
+	
+	override public function onResize(x:Int, y:Int, w:Int, h:Int) 
+	{
+		if ( _mc_item != null ) {
+			Tool.centerForce( _mc_item, 1366, 768, x, y, w, h, 0.5, 0.2 );
+		}
+		if ( _mc_popup != null ) {
+			Tool.center(_mc_popup, x, y, w, h);
+		}
+		if ( _mc_back != null ) {
+			_mc_back.width = w;
+			_mc_back.height = h;
+		}
 	}
 }
