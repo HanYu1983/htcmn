2d044c5e833bef704b1c5a404e36c564737b556b
HanYu1983 <hanyushome@gmail.com>
2015-03-09 21:36:55 +0800

    修改etm

--- 20140225_2/src/control/OnMessageBtnClick.hx
+++ 20140225_2/src/control/OnMessageBtnClick.hx
@@ -1,6 +1,9 @@
 package control;
 
+import flash.errors.Error;
 import model.AppAPI;
+import model.ETMAPI;
+import org.han.Async;
 import org.vic.web.WebCommand;
 import view.fb.DetailFromPopup;
 import view.MessagePopup;
@@ -28,14 +31,29 @@ class OnMessageBtnClick extends WebCommand
 		
 		var func:Dynamic = {
 			btn_onMessageBtnClick_confirm: function() {
-				AppAPI.openPage( {
-					mgr:getWebManager(),
-					page: DetailFromPopup,
-					params: null
-					
-				} ) (null);
 				
-				closeMessagePopop();
+				SimpleController.onHttpLoadingStart();
+				
+				function handleNext( err:Error, ret:Dynamic ){
+					if ( err != null ) {
+						SimpleController.onError( err.message );
+					}
+					SimpleController.onHttpLoadindEnd();
+					closeMessagePopop();
+				}
+				
+				Async.waterfall([
+					ETMAPI.isEnterInfo,
+					function getData( args: { isWritten:Bool, token: String } ) {
+						return function( cb:Dynamic ) {
+							getWebManager().setData('etmToken', args.token);
+							cb( null, { mgr:getWebManager(), page:DetailFromPopup, params:null } );
+						}
+					},
+					AppAPI.openPage
+					
+				], handleNext, { fbid: '', email: '' } );
+				
 				
 			},
 			btn_onMessageBtnClick_cancel: function() {
