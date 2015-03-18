3a84bc40df270a916cf4c9c52afc92aecd1008e1
HanYu1983 <hanyushome@gmail.com>
2015-03-19 06:00:26 +0800

    add sound mediator

--- 20140225_2/src/TestSound.hx
+++ 20140225_2/src/TestSound.hx
@@ -1,12 +1,16 @@
 package ;
 import flash.display.Stage;
 import flash.events.Event;
+import flash.events.KeyboardEvent;
 import flash.events.MouseEvent;
 import flash.Lib;
 import flash.media.Sound;
 import flash.media.SoundChannel;
 import flash.media.SoundTransform;
 import flash.net.URLRequest;
+import flash.ui.Keyboard;
+import haxe.Timer;
+import model.DolbySoundMediator;
 
 /**
  * ...
@@ -20,6 +24,35 @@ class TestSound
 
 	public function new() 
 	{
+		test2();
+	}
+	
+	function test2() {
+		
+		var mediator = new DolbySoundMediator( SoundType.Normal );
+		
+		mediator.load( { htc:"sound/dolby.mp3", other:"sound/other.mp3" }, function() {
+			
+			mediator.play();
+			var currtime = 0.0;
+			
+			Lib.current.stage.addEventListener( KeyboardEvent.KEY_UP, function( e:KeyboardEvent ) {
+				
+				switch(e.keyCode) {
+					case Keyboard.LEFT | Keyboard.RIGHT:
+						mediator.toggle();
+					case Keyboard.UP:
+						mediator.play( currtime );
+					case Keyboard.DOWN:
+						currtime = mediator.stop();
+				}
+				
+			});
+			
+		} );
+	}
+	
+	function test1() {
 		var sa = new Sound();
 		var so:SoundChannel;
 		sa.addEventListener( Event.COMPLETE, function( e ) {
