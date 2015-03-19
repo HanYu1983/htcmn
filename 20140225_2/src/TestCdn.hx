953d533ec8ac89f1ab7ec8dd1913a4422e460858
vic <finalsteve@hotmail.com>
2015-03-19 11:58:31 +0800

    update

--- 20140225_2/src/TestCdn.hx
+++ 20140225_2/src/TestCdn.hx
@@ -1,5 +1,16 @@
 package ;
+import flash.display.Loader;
+import flash.errors.Error;
+import flash.events.Event;
+import flash.events.ProgressEvent;
+import flash.events.SecurityErrorEvent;
 import flash.Lib;
+import flash.net.URLRequest;
+import flash.system.LoaderContext;
+import flash.system.Security;
+import flash.system.SecurityDomain;
+import flash.text.TextField;
+import flash.ui.Keyboard;
 import org.vic.web.WebManager;
 import view.IntroPage;
 
@@ -12,10 +23,34 @@ class TestCdn
 
 	public function new() 
 	{
-		WebManager.inst.init( Lib.current.stage );
-		WebManager.inst.addLayer( 'page' );
+		Security.allowDomain("*");
+		Security.allowInsecureDomain("*");
+		var loader = new Loader();
+		var context:LoaderContext = new LoaderContext(true);
+		//context.checkPolicyFile = true;
 		
-		WebManager.inst.openPage( IntroPage, null );
+		//loader.load( new URLRequest( 'http://eda9962ce4da9c47c32c-6a163228e67308da9664e4f095f00920.r12.cf6.rackcdn.com/src/TechDouble.swf' ));
+		loader.load( new URLRequest( 'http://eda9962ce4da9c47c32c-6a163228e67308da9664e4f095f00920.r12.cf6.rackcdn.com/src/TechDolby.swf' ), context);
+		//loader.load( new URLRequest( 'src/TechDolby.swf' ));
+		loader.contentLoaderInfo.addEventListener(ProgressEvent.PROGRESS, function( e ) {
+			//trace( e );
+			//txt_per.text = e;
+		});
+		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, function( e ) {
+			trace("DDFFBB");
+			try{
+			trace( "content", loader.contentLoaderInfo.content );
+			Lib.current.stage.addChild( loader.contentLoaderInfo.content );
+			}catch ( e:Error ) {
+				trace( e.getStackTrace() );
+				trace( "content", loader.contentLoaderInfo );
+			}
+		});
+		
+		
+		loader.contentLoaderInfo.addEventListener(SecurityErrorEvent.SECURITY_ERROR, function( e ) {
+			trace( e );
+		});
 	}
 	
 	static function main() {
