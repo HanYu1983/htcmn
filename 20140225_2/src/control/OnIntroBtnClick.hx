3f2da7181f1903f86ebb69ae2e48bb96cddc51a9
vic <finalsteve@hotmail.com>
2015-03-22 01:16:45 +0800

    update

--- 20140225_2/src/control/OnIntroBtnClick.hx
+++ 20140225_2/src/control/OnIntroBtnClick.hx
@@ -1,4 +1,5 @@
 package control;
+import helper.Tool;
 import model.AppAPI;
 import org.vic.web.WebCommand;
 import view.TechPage;
@@ -12,13 +13,23 @@ class OnIntroBtnClick extends WebCommand
 
 	override public function execute(?args:Dynamic):Void 
 	{
-		AppAPI.changePage( 
+		var goto:Dynamic = {
+			btn_onIntroBtnClick_enter: function() {
+				AppAPI.changePage( 
 					{ 
 						mgr:this.getWebManager(), 
 						page: TechPage, 
 						params: { } 
 						
 					}) (null);
+			},
+			btn_onIntroBtnClick_early: function(){
+				var url = getWebManager().getData( 'config' ).url.early;
+				Tool.getURL(url);
+			}
+		}
+		var targetPage:String = args[1].name;
+		Reflect.field(goto, targetPage)();
 	}
 	
 }
