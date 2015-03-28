58fb63ac734ed4731b46b1dd853d149c8117bec0
vic <finalsteve@hotmail.com>
2015-03-28 18:06:29 +0800

    update

--- 20140225_2/src/view/tech/TechBoom.hx
+++ 20140225_2/src/view/tech/TechBoom.hx
@@ -1,4 +1,6 @@
 package view.tech;
+import flash.display.DisplayObject;
+import org.vic.utils.BasicUtils;
 
 /**
  * ...
@@ -6,11 +8,36 @@ package view.tech;
  */
 class TechBoom extends DefaultTechPage
 {
+	var mc_phone:DisplayObject;
+	var mc_finger:DisplayObject;
+	var ary_wave:Array<DisplayObject> = [];
 
 	public function new() 
 	{
 		super();
 	}
+	
+	override function forScript(e) 
+	{
+		super.forScript(e);
+		
+		BasicUtils.revealObj( getRoot(), function( disobj:DisplayObject ) {
+			switch( disobj.name ) {
+				case 'mc_phone':
+					mc_phone = disobj;
+				case 'mc_wave_01', 'mc_wave_02', 'mc_wave_03', 'mc_wave_04', 'mc_wave_05',
+					 'mc_wave_06', 'mc_wave_07', 'mc_wave_08', 'mc_wave_09', 'mc_wave_10':
+						ary_wave.push( disobj );
+			}
+		});
+		
+		getWebManager().log( mc_phone );
+		getWebManager().log( ary_wave );
+		
+		mc_finger = getLoaderManager().getTask( 'TechConnect' ).getObject( 'Finger' );
+		getRoot().addChild( mc_finger );
+	}
+	
 	override function getSwfInfo():Dynamic 
 	{
 		var config:Dynamic = getWebManager().getData( 'config' );
--- 20140225_2/src/view/tech/TechPhoto.hx
+++ 20140225_2/src/view/tech/TechPhoto.hx
@@ -1,4 +1,7 @@
 package view.tech;
+import flash.display.DisplayObject;
+import flash.display.MovieClip;
+import org.vic.utils.BasicUtils;
 
 /**
  * ...
@@ -6,10 +9,31 @@ package view.tech;
  */
 class TechPhoto extends DefaultTechPage
 {
+	var ary_effect:Array<MovieClip> = [];
+	var ary_btn:Array<MovieClip> = [];
 
 	public function new() 
 	{
 		super();
+	}
+	
+	override function forScript(e) 
+	{
+		super.forScript(e);
+		
+		BasicUtils.revealObj( getRoot(), function( disobj:DisplayObject ) {
+			switch( disobj.name ) {
+				case 'mc_effect_01', 'mc_effect_02', 'mc_effect_03',
+					 'mc_effect_04', 'mc_effect_05', 'mc_effect_06':
+						ary_effect.push( cast( disobj, MovieClip ));
+				case 'btn_effect_01', 'btn_effect_02', 'btn_effect_03',
+					 'btn_effect_04', 'btn_effect_05', 'btn_effect_06':
+						ary_btn.push( cast( disobj, MovieClip ));
+			}
+		});
+		trace( ary_effect );
+		trace( ary_btn );
+		
 	}
 	
 	override function getSwfInfo():Dynamic 
