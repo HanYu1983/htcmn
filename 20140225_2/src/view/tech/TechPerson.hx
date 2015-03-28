788eb03711ce42fffa864c66bf0b22adbdb4b7cf
HanYu1983 <hanyushome@gmail.com>
2015-03-28 18:27:40 +0800

    update

--- 20140225_2/src/view/tech/TechPerson.hx
+++ 20140225_2/src/view/tech/TechPerson.hx
@@ -10,7 +10,16 @@ class TechPerson extends DefaultTechPage
 	public function new() 
 	{
 		super();
-		this.createDebugRoot("person");
 	}
 	
+	override function getSwfInfo():Dynamic 
+	{
+		var config:Dynamic = getWebManager().getData( 'config' );
+		return {name:'TechTheme', path:config.swfPath.TechTheme[ config.swfPath.TechTheme.which ] };
+	}
+	
+	override function getRootInfo():Dynamic 
+	{
+		return {name:'TechTheme', path:'mc_anim' };
+	}
 }
