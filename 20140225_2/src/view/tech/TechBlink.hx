8eed0a50d280711e706addc233f051f8014c06d8
vic <finalsteve@hotmail.com>
2015-03-29 14:47:59 +0800

    update

--- 20140225_2/src/view/tech/TechBlink.hx
+++ 20140225_2/src/view/tech/TechBlink.hx
@@ -1,4 +1,7 @@
 package view.tech;
+import flash.display.DisplayObject;
+import flash.display.MovieClip;
+import org.vic.utils.BasicUtils;
 
 /**
  * ...
@@ -6,11 +9,39 @@ package view.tech;
  */
 class TechBlink extends DefaultTechPage
 {
+	var mc_rest:MovieClip;
+	var btn_screen:MovieClip;
 
 	public function new() 
 	{
 		super();
-		this.createDebugRoot("blink");
 	}
 	
+	override function forScript(e) 
+	{
+		super.forScript(e);
+		
+		BasicUtils.revealObj( getRoot(), function( disobj:DisplayObject ) {
+			switch( disobj.name ) {
+				case 'mc_rest':
+					mc_rest = cast( disobj, MovieClip );
+				case 'btn_screen':
+					btn_screen = cast( disobj, MovieClip );
+			}
+		});
+		
+		getWebManager().log( mc_rest );
+		getWebManager().log( btn_screen );
+	}
+	
+	override function getSwfInfo():Dynamic 
+	{
+		var config:Dynamic = getWebManager().getData( 'config' );
+		return {name:'TechBlink', path:config.swfPath.TechBlink[ config.swfPath.TechBlink.which ] };
+	}
+	
+	override function getRootInfo():Dynamic 
+	{
+		return {name:'TechBlink', path:'mc_anim' };
+	}
 }
